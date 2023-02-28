5e4281da5497c8af422d9bb11592587ccb61b5dd
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-02-28 22:04:53 +0300

    написана программа для task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,4 +1,4 @@
-﻿void PrintArr(string[] array)
+﻿void PrintArray(string[] array)
 {
     for (int i = 0; i< array.Length; i++)
         Console.Write($"{array[i]} ");
@@ -19,8 +19,8 @@ void SelectionArray(string[] array1, string[] array2)
 string[] array1 = new string[5] { "array", "arr", "123", "12345", "proekt" };
 string[] array2 = new string[array1.Length];
 Console.WriteLine("Вам представлен заданный массив состоящий из 5 элементов");
-PrintArr(array1);
+PrintArray(array1);
 Console.WriteLine("\nПосле выполнения данной программы во втором массиве останутся только элементы длина которых равна 3 символа или меньше");
 SelectionArray(array1, array2);
-PrintArr(array2);
+PrintArray(array2);
 
