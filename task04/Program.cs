d03738e29a0da962e8c981074f4659aaef82cf8c
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-02-28 22:53:48 +0300

    загрузил блок-схему

--- task04/Program.cs
+++ task04/Program.cs
@@ -6,17 +6,17 @@
 }
 void SelectionArray(string[] array1, string[] array2)
 {
-    int num = 0;
+    int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
-            array2[num] = array1[i];
-            num++;
+            array2[count] = array1[i];
+            count++;
         }
     }
 }
-string[] array1 = new string[5] { "array", "arr", "123", "12345", "proekt" };
+string[] array1 = new string[5] { "14array88", "arr", "666", "543212345", "8====э" };
 string[] array2 = new string[array1.Length];
 Console.WriteLine("Вам представлен заданный массив состоящий из 5 элементов");
 PrintArray(array1);
